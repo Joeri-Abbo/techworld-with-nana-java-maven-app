4c8b93f05c33c7c1458bbb58315bf007ac612686
import-Hammad <hammadmalik67000@gmail.com>
2026-03-08 15:39:58 +0500

    Refactor build functions for consistency and clarity

--- script.groovy
+++ script.groovy
@@ -1,16 +1,21 @@
-def buildjar() {
+def buildJar() {
     echo 'building the application...'
-        sh 'mvn package'
+    sh 'mvn package'
 }
 
-def buildimage() {
-    echo  "building  the docker image"
-                    withCredentials([usernamePassword(credentialsId:'docker-hub-repo',  passwordVariable: 'PASS', usernameVariable: 'USER')]){
-                        sh  'docker build -t piratehammad/demo-app:jma-2.0 .'
-                        sh "echo  $PASS  | docker login -u $USER --password-stdin"
-                        sh 'docker push piratehammad/demo-app:jma-2.0'
-                    }
+def buildImage() {
+    echo "building the docker image"
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-hub-repo',
+        passwordVariable: 'PASS',
+        usernameVariable: 'USER'
+    )]) {
+        sh 'docker build -t piratehammad/demo-app:jma-2.0 .'
+        sh "echo \$PASS | docker login -u \$USER --password-stdin"
+        sh 'docker push piratehammad/demo-app:jma-2.0'
+    }
 }
+
 def deployApp() {
     echo 'deploying the application...'
 }
